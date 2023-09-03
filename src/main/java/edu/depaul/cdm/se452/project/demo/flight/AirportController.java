d5c5e52f75ee8f764f111174c6fb1d651355639e
Ken Yu <kyu@cdm.depaul.edu>
2023-09-03 18:39:30 -0500

    service and controller separatation

--- src/main/java/edu/depaul/cdm/se452/project/demo/flight/AirportController.java
+++ src/main/java/edu/depaul/cdm/se452/project/demo/flight/AirportController.java
@@ -3,6 +3,10 @@ package edu.depaul.cdm.se452.project.demo.flight;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,27 +15,28 @@ import lombok.extern.log4j.Log4j2;
 @RestController
 @RequestMapping("/api/airports")
 @Log4j2
-public class AirportService {
+public class AirportController {
     @Autowired
-    private AirportRepository repo;
+    private AirportService service;
 
+    @GetMapping
     public List<Airport> list() {
-        log.traceEntry("Enter list");
-        var retval = repo.findAll();
-        log.traceExit("Exit list", retval);        
-        return repo.findAll();
+        return service.list();
     }
 
-    public void save(Airport Airport) {
+    @PostMapping
+    public void save(@RequestBody Airport Airport) {
         log.traceEntry("enter save", Airport);
-        repo.save(Airport);
+        service.save(Airport);
         log.traceExit("exit save", Airport);                
     }
 
 
+    @DeleteMapping
     public void delete(String code) {
         log.traceEntry("Enter delete", code);
-        repo.deleteById(code);
+        service.delete(code);
         log.traceExit("Exit delete");
     }
+    
 }
